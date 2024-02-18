36ea76561707674edd3c044d5fa403d6b03a3c4e
HynekSvoboda <97514770+HynekSvoboda@users.noreply.github.com>
2024-02-18 13:10:26 +0100

    cors

--- backend.js
+++ backend.js
@@ -6,11 +6,11 @@ const http = require('http')
 const server = http.createServer(app)
 const { Server } = require('socket.io')
 const io = new Server(server, { pingInterval: 2000, pingTimeout: 5000 })
-
+const cors =require("cors");
 const port = 8080
 
 app.use(express.static('public'))
-
+app.use(cors())
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
