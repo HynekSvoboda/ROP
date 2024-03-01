b89a32ac266328d4ecf1b28fea2e92f250edb361
HynekSvoboda <97514770+HynekSvoboda@users.noreply.github.com>
2024-03-01 08:27:58 +0100

    Update eventListeners.js

--- public/js/eventListeners.js
+++ public/js/eventListeners.js
@@ -1,12 +1,9 @@
-// Define a variable to store the last shot time
 let lastShotTime = 0;
 
-// Add click event listener
 addEventListener('click', (event) => {
   const now = Date.now();
-  const delayBetweenShots = 500; // Set the desired delay between shots in milliseconds
+  const delayBetweenShots = 500;
 
-  // Check if enough time has passed since the last shot
   if (now - lastShotTime > delayBetweenShots) {
     const canvas = document.querySelector('canvas');
     const { top, left } = canvas.getBoundingClientRect();
@@ -26,9 +23,6 @@ addEventListener('click', (event) => {
       angle
     });
 
-    // Update the last shot time
     lastShotTime = now;
-
-    // console.log(frontEndProjectiles);
   }
 });
